59b0ce05f1f3aeb8de49770abe668ad03bf4697b
visionspeaker <yooseok.kang@gmail.com>
2026-07-15 23:29:55 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'hyele-app-v11';
+const CACHE = 'hyele-app-v12';
 const ASSETS = ['./', './index.html', './manifest.webmanifest',
   './icon-192.png', './icon-512.png', './apple-touch-icon-180.png'];
 self.addEventListener('install', e => {
