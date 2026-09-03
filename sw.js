5831d3065955b597fb6931652ce98e6d1892c68c
visionspeaker <yooseok.kang@gmail.com>
2026-09-03 10:17:09 +0900

    Update cache version from v46 to v47

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'hyele-app-v46';
+const CACHE = 'hyele-app-v47';
 const ASSETS = ['./', './index.html', './manifest.webmanifest',
   './icon-192.png', './icon-512.png', './apple-touch-icon-180.png'];
 self.addEventListener('install', e => {
